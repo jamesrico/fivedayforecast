e76694abb68b16bec3873eac6ae77ef43a65ad3c
Bryan Finlayson <bdfinlayson@gmail.com>
2015-01-28 21:36:57 -0600

    added replace method for creating custom url

--- app/scraps.js
+++ app/scraps.js
@@ -24,3 +24,81 @@ function createList(namesArray){
   return docFragment;
 }
 
+
+/* browser: true */
+
+'use strict';
+
+document.querySelector('h1').classList.add('red');
+
+var h2s = document.querySelectorAll('h2');
+for (var i=0; i < h2s.length; i++) {
+  h2s[i].classList.add('blue');
+}
+
+// h3s toUpperCase(); 'hello'.toUpperCase() => 'HELLO'
+var h3s = document.querySelectorAll('h3');
+// for (i=0; i < h3s.length; i++) {
+//   h3s[i].textContent = h3s[i].textContent.toUpperCase();
+// }
+
+// [] = Array.prototype
+[].forEach.call(h3s, function(el){
+  el.textContent = el.textContent.toUpperCase();
+})
+
+var target = document.querySelector('.target');
+var elementString = '<h2>OMG <a href="http://google.com">GOOG</a></h2>'
+var imgCat = '<img src="http://www.washingtonpost.com/news/morning-mix/wp-content/uploads/sites/21/2014/09/Grumpy_Cat_Endorsement-017d7-ULFU.jpg">
+
+document.querySelector('#add-element').addEventListener('click', function(){
+  target.insertAdjacentHTML('afterend', elementString);
+  target.appendChild(createSuperLimeDiv("OMG OMG OMG"));
+});
+
+function createSuperLimeDiv(text){
+  var docFragment = document.createDocumentFragment();
+
+  var supergreen = document.createElement('div');
+  supergreen.setAttribute("class", "supergreen");
+  docFragment.appendChild(supergreen);
+
+  var superyellow = document.createElement('div');
+  superyellow.setAttribute("class", "superyellow");
+  supergreen.appendChild(superyellow);
+  var superyellow_text = document.createTextNode(text);
+  superyellow.appendChild(superyellow_text);
+
+  return docFragment;
+}
+
+
+
+
+setInterval(function () {
+  setRandomColor(document.querySelector('h1'))
+}, 1000)
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 256);
+}
+
+function setRandomColor(element){
+  element.style.backgroundColor = randomColor();
+}
+
+function randomColor() {
+  var red = getRandomColor();
+  var green = getRandomColor();
+  var blue = getRandomColor();
+  return 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+
+
+document.querySelector('#add-image').addEventListener('click', function(){
+  target.insertAdjacentHTML('afterend', imgCat);
+  target.appendChild(createSuperLimeDiv("New Image Created!"));
+});
+
+
--- app/zipcode.js
+++ app/zipcode.js
@@ -1,24 +1,28 @@
+'use strict';
 
+var customUrl;
 
 document.querySelector('#submitButton').addEventListener('click', function() {
-  userZipCode = document.querySelector('#userZipCode').value;
+  var userZipCode = document.querySelector('#userZipCode').value;
+  var strUrl = 'https://api.wunderground.com/api/152eccaad753eb4b/forecast10day/q/37207.json';
+  customUrl = strUrl.replace('37207', userZipCode);
   console.log(userZipCode);
-  zipCodeUrl = 'https://api.wunderground.com/api/152eccaad753eb4b/forecast10day/q/37207.json';
+  console.log(customUrl);
+  var zipCodeUrl = 'https://api.wunderground.com/api/152eccaad753eb4b/forecast10day/q/37207.json';
   console.log(zipCodeUrl);
-
-
-  getJSON(zipCodeUrl, function(result) {
-    var day = result.forecast.simpleforecast.forecastday;
-      console.log(day);
-    var dailyHigh = result.forecast.simpleforecast.forecastday.high;
-      console.log(dailyHigh);
-    var dailyLow = day[i].low.fahrenheit;
-    var weekday = day[i].date.weekday_short;
-    var icon = day[i].icon_url;
-    console.log(day + '' + dailyHigh + '' + dailyLow + '' + weekday + '');
   });
-});
 
+
+
+//  getJSON(customUrl, function(result) {
+//    var day = _.values.forecast.simpleforecast.forecastday;
+//      console.log(day);
+//    var dailyHigh = value.forecast.simpleforecast.forecastday.high;
+//      console.log(dailyHigh);
+//    var dailyLow = day[i].low.fahrenheit;
+//    var weekday = day[i].date.weekday_short;
+//    var icon = day[i].icon_url;
+//    console.log(day + '' + dailyHigh + '' + dailyLow + '' + weekday + '');
 
 
 
@@ -34,5 +38,7 @@ function getJSON(url, cb){
 
   request.send();
 }
+
+
 
 
